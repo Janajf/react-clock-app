57f66baec59ab5223ba6ef9536f3ce3cdcb24c7a
student11 <student11@Codes-Air-76.hsd1.de.comcast.net>
2023-07-16 12:55:39 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './styles.css'
+import Clock from "./components/AnalogClock.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Clock/>
+    </>
+  )
+}
+
+export default App
